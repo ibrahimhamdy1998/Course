7f3eba00450b48fc4cec71c642c964297d4a3b70
ibrahimhamdy1998 <ibrahimhamdy1998@gmail.com>
2021-08-25 22:40:45 +0200

    Presenting data

---
+++ student/WebForm2.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm2.aspx.cs" Inherits="student.WebForm2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Student Data</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+</head>
+<body>
+   <nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Student'S Data</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#">Registration</a></li>
+    </ul>
+  </div>
+</nav>
+    <form id="form1" runat="server">
+        <div style="margin-left:100px; margin-right:100px;">
+            <asp:GridView ID="Student_ID" CssClass="table table-hover table-striped"   GridLines="None" AutoGenerateColumns="false" runat="server" >
+                   <Columns>
+                            <asp:BoundField DataField="ID" HeaderText="Sudent ID" />
+                            <asp:BoundField DataField="Fname" HeaderText="First Name" />
+                            <asp:BoundField DataField="Lname" HeaderText="Last Name" />
+                            <asp:BoundField DataField="Age" HeaderText="Age" />
+                            <asp:BoundField DataField="Email" HeaderText="Email" />
+                            <asp:BoundField DataField="Accpetance" HeaderText="Registration Date" />
+                            <asp:BoundField DataField="Is_accepted" HeaderText="Accepted Or Not" />
+                        </Columns>
+            </asp:GridView>
+                       <asp:GridView runat="server" ID="ss"  CssClass="table table-hover table-striped"    GridLines="None" ></asp:GridView>
+            
+            
+        </div>
+    </form>
+</body>
+</html>
---
+++ student/WebForm2.aspx.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Web.UI.WebControls;
+namespace student
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        private object dataGridView;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           if (!Page.IsPostBack)
+            {
+            data();
+                dataS();
+            
+            }
+        }
+
+        private void data()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["myconnection"].ToString();
+           SqlConnection sqlConnection = new SqlConnection(connectionString);
+         
+            SqlCommand cmd = new SqlCommand("select * from Student", sqlConnection);
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            
+            sda.Fill(ds);
+            
+            Student_ID.DataSource = ds;
+            Student_ID.DataBind();
+                
+
+            
+            }
+        private void dataS()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["myconnection"].ToString();
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+            SqlCommand cmd = new SqlCommand("select Specialization From Specialization INNER JOIN Student ON Specialization.SPECIALIZATION_ID=Student.SPECIALIZATION_ID", sqlConnection);
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+
+            sda.Fill(ds);
+
+            ss.DataSource = ds;
+            ss.DataBind();
+
+
+
+        }
+        private void dataG()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["myconnection"].ToString();
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+            SqlCommand cmd = new SqlCommand("select * from Student", sqlConnection);
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+
+            sda.Fill(ds);
+
+            Student_ID.DataSource = ds;
+            Student_ID.DataBind();
+
+
+
+        }
+        private void dataC()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["myconnection"].ToString();
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+            SqlCommand cmd = new SqlCommand("select * from Student", sqlConnection);
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+
+            sda.Fill(ds);
+
+            Student_ID.DataSource = ds;
+            Student_ID.DataBind();
+
+
+
+        }
+
+
+
+
+    }
+
+}
+
